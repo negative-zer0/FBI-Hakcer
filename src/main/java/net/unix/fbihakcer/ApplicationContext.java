1f4d6db6c6487ce8ebdf885f456ec40e002a64c5
negative-zer0 <iunixpl69@gmail.com>
2021-04-19 18:12:40 +0200

    based

--- src/main/java/net/unix/fbihakcer/ApplicationContext.java
+++ src/main/java/net/unix/fbihakcer/ApplicationContext.java
@@ -1,6 +1,7 @@
 package net.unix.fbihakcer;
 
 import net.unix.fbihakcer.util.ConditionUtil;
+import java.util.stream.IntStream;
 
 /**
  * @author Unix
@@ -11,11 +12,12 @@ public class ApplicationContext {
 
     public static void main(String[] args) {
         println("Starting Hack...");
-        for (int i = 0; i < 120; ++i) {
+
+        IntStream.range(0, 120).forEachOrdered(i -> {
             final int value = i;
             ConditionUtil.check(i % 20 == 0, () -> println("Hacking FBI " + value + "%"));
             sleep(100L);
-        }
+        });
 
         println("FBI Hacked Successfully");
     }
