1a4b7f25e388c614878c4a610d95e674e2f1d3ca
negative-zer0 <iunixpl69@gmail.com>
2021-04-20 08:24:17 +0200

    some changes

--- src/main/java/net/unix/fbihakcer/ApplicationContext.java
+++ src/main/java/net/unix/fbihakcer/ApplicationContext.java
@@ -13,12 +13,13 @@ public class ApplicationContext {
     public static void main(String[] args) {
         println("Starting Hack...");
 
-        IntStream.range(0, 120).forEachOrdered(i -> {
+        IntStream.rangeClosed(0, 100).forEachOrdered(i -> {
             final int value = i;
             ConditionUtil.check(i % 20 == 0, () -> println("Hacking FBI " + value + "%"));
             sleep(100L);
         });
 
+        sleep(200L);
         println("FBI Hacked Successfully");
     }
 
